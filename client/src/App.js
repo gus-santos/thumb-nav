ff491bc36a9c59706871bcd1c0cf7d2322fd55b5
Gus <santos.gus@protonmail.com>
2021-06-06 19:36:39 -0700

    Rename Components folder to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './styles/main.min.css';
 
-import NavBanner from './Components/NavBanner/NavBanner';
-import Content from './Components/Content/Content';
-import Nav from './Components/Nav/Nav';
+import NavBanner from './components/NavBanner/NavBanner';
+import Content from './components/Content/Content';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
